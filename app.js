0c5de67d251a26cdd78dd00bb55c0554b5a12653
Tejas Weldode <112164797+Tejas-Weldode@users.noreply.github.com>
2024-10-25 01:24:50 +0530

    changed uri

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ app.use(bodyParser.json());
 
 // MongoDB Connection
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to Amazon DocumentDB"))
     .catch((err) => console.error("DocumentDB connection error:", err));
 
